c542a8f9d5c99588f35035ee7fa413ecc5a021c0
fatma <fatma.abdelalim2016@gmail.com>
2023-06-20 18:41:59 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,10 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * _islower - function to check if
- *           character is lowercase
- *
- * 0c: checks input of function
- *
- * Return: return 1 if 'c' is lowercase
- *         otherwise always 0 (Success)
- */
+#include <stdio.h>
 
-int _islower(int c)
-{       
-        if (c >= 97 && c <= 122)
-                return (1);
-        return (0);
-}
+int _putchar(char c);
+
+
+
+#endif
