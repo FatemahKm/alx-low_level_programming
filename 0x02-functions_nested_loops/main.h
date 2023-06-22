bd580198423631e19996a07badfeed882f5ed9c3
fatma <fatma.abdelalim2016@gmail.com>
2023-06-21 19:45:05 -0700

    pp

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,10 +13,11 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int _putchar(char c);
+void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
 
 
 #endif
