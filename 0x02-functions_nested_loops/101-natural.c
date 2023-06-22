f2464bfa4ba61e42bbd2bc95d84784edb9baaf91
fatma <fatma.abdelalim2016@gmail.com>
2023-06-21 18:59:44 -0700

    pp

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,39 +1,25 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_times_table - the function that prints the n times table.
+ * main - Entry point
  *
- * @n: function input.
+ * this is a program that compute and prints-
+ *
+ * the sum of all the multiples of 3 or 5 below 1024.
+ *
+ * Return: 0 (Success).
  */
 
-void print_times_table(int n)
+int main(void)
 {
-	int i, mult, prod;
+	int i, sum;
 
-	if (n <= 15 && n >= 0)
+	for (i = 1; i <= 1024; i++)
 	{
-		for (i = 0; i <= n; ++i)
-		{
-			_putchar(48);
-			for (mult = 1; mult <= n; ++mult)
-			{
-				_putchar(',');
-				_putchar(' ');
-				prod = i * mult;
-				if (prod <= 9)
-					_putchar(' ');
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + 48);
-					_putchar((prod / 10) % 10 + 48);
-				}
-				else if (prod <= 99 && prod >= 10)
-					_putchar((prod / 10) + 48);
-				_putchar((prod % 10) + 48);
-			}
-			_putchar('\n');
-		}
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
 	}
+	printf("%d\n", sum);
+	return (0);
 }
