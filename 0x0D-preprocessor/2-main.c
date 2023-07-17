4df3aaea0b06b121a003826d2ce9b77fcd273efc
fatma <fatma.abdelalim2016@gmail.com>
2023-07-17 15:26:51 -0700

    a

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - print name of of this source file
+ * main - print name  of this source file
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	printdf("%s\n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
