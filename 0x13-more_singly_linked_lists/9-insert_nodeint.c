f57a251ba1ab71491701844d9e09660172526be1
fatma <fatma.abdelalim2016@gmail.com>
2023-08-01 15:39:22 -0700

    a

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,7 +8,7 @@
  *
  * Return: address of new node
  */
-list_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *node, *new_node = malloc(sizeof(listint_t));
 	unsigned int i = 0;
