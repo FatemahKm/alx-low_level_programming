e3729636ca03e66dd80a556df8d62342a5f97f42
fatma <fatma.abdelalim2016@gmail.com>
2023-08-01 18:31:26 -0700

    a

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -3,14 +3,14 @@
 
 /**
  * find_listint_loop - finds a loop in a linked list
- * 
+ *
  * @head: linked list to search
  *
  * Return: address of node where loop starts/returns, NULL if no loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *ptr, end;
+	listint_t *ptr, *end;
 
 	if (head == NULL)
 		return (NULL);
